f6e4dc183691399672f25196abbde62a6ab2273d
Matt Felsen <mattfelsen@gmail.com>
2013-01-30 23:35:30 -0500

    Write routing code and break files into views & partials

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php phpinfo(); ?>
+<?php
+
+$uri = explode('/', $_SERVER['REQUEST_URI']);
+
+// Get rid of first, blank element
+array_shift($uri);
+
+// Get rid of "beta" element since the site is in development!
+array_shift($uri);
+
+$page = array_shift($uri);
+
+include('./views/header.html');
+
+if ($page != 'projects') {
+	include('./views/main.html');
+} else {
+	$project = array_shift($uri);
+	include("./views/$project.html");
+}
+
+include('./views/footer.html');
+
+?>
