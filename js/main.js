2165bad12af753141a15dc656ae42ae01989cd19
Matt Felsen <mattfelsen@gmail.com>
2013-01-30 19:40:31 -0500

    Initial commit. Not a finished copy, just want to start versioning

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Clicking project thumbnails slides carousel to that item
+$('.project-thumbnails img').click(function() {
+	$('.carousel').carousel($(this).parent().index());
+});
+
+// Handle fetching of content & animating old/new content
+function animateIn(url) {
+	$('#wrap').animate({
+		opacity: 0,
+		'margin-top': '-150px'
+	}, 'fast', function() {
+		$.get(url, function(data) {
+			window.scrollTo(0, 0);
+			$('#wrap').html(data);
+			$('#wrap').css('margin-top', '150px');
+			$('#wrap').animate({
+				opacity: 1,
+				'margin-top': 0
+			}, 'fast');
+		});
+	});
+}
